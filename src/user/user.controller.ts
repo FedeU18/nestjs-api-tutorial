667cc0e9432d43c7aea645cc80664f9d4172ac8d
Federico <fedee.unates.2001@gmail.com>
2024-02-07 00:01:35 -0300

    ruta /users/me para obtener un usuario

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import {
+  Controller,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('users')
+export class UserController {
+  // => /users/me
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe() {
+    return 'user info';
+  }
+}
